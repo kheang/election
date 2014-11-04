405dc0fe5e6308d0d8ef23ea2e01cef4b1e278eb
Kheang Lim <kheang@gmail.com>
2014-11-04 13:47:07 -0500

    Basic rails-api project with functioning candidates

---
+++ app/controllers/v1/candidates_controller.rb
@@ -0,0 +1,18 @@
+class V1::CandidatesController < ApplicationController
+	before_filter :load_candidate, only: [:show]
+
+  def index
+		@candidates = Candidate.all
+		render json: @candidates
+  end
+
+  def show
+		render json: @candidate, only: [:name,:party]
+  end
+
+	private
+
+	def load_candidate
+		@candidate = Candidate.find(params[:id])
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ElectionApi::Application.config.secret_token = '94b94d085619aa207715eae12ed8e68a6521e731282747eccabd27ca54e78efb983895f216bae3c87b21395c651c9ad473a439f287d51653b68e1137b4728913'
